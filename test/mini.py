4e0f49a93e336a0c04752952836f4d235493e804
Mel <Mel1853520053@163.com>
2019-06-11 21:05:05 +0800

    立项

---
+++ test/mini.py
@@ -0,0 +1,35 @@
+# import socket
+# import threading
+# import sys
+# class HttpWedServer(object):
+#     def __init__(self, port):
+#         tcp_
+
+# import socket
+# # if  __name__ == '__main__':
+# tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# tcp_socket.connect(('192.168.16.130)', 8080))
+# input()
+# tcp_socket.close()
+
+
+# from redis import *
+# if __name__ == '__main__':
+#     try:
+#         sr = StrictRedis()
+#         result=sr.set('name', 'itheima')
+#         print(result)
+#     except Exception as e:
+#         print(e)
+
+
+# from redis import *
+# if __name__ == '__main__':
+#     try:
+#         sr= StrictRedis()
+#
+#         result =sr.keys()
+#         print(result)
+#     except Exception as e:
+#         print(e)
+
